cb2ea21f7e6efe38b7c2592b42f9b3e8121f5e55
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2024-10-11 15:19:54 -0400

    added & stylized screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav"
+import Screen from "./components/Screen"
 
 function App () {
   return (
     <main className="calculator-app theme-1">
       <div className="wrapper">
         <Nav /> 
+        <Screen />
       </div>
     </main>
   )
---
+++ src/components/Screen.tsx
@@ -0,0 +1,9 @@
+function Screen () {
+  return (
+    <section className="screen">
+      399,981
+    </section>
+  )
+}
+
+export default Screen
